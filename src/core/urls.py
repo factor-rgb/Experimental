924771ad067eba6ca1c4d0e3482ad925abfecd59
Raul Vieira <raulvieira199@gmail.com>
2025-03-30 13:17:24 -0500

    feat(core):mes improve to meses add create

--- src/core/urls.py
+++ src/core/urls.py
@@ -5,6 +5,6 @@ app_name = "core"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("agenda/crear acontecimiento/", views.AgendaCreateView.as_view(), name="agregarfecha"),
     path("agenda/mes/", views.mes, name="mes"),
+    path("agenda/prueba/", views.prueba, name="prueba"),
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Agenda
 from .forms import AgendaForm
 
@@ -11,12 +11,17 @@ def index(request):
       context = {"fechas": query}
       return render(request, "core/index.html", context)
 
-class AgendaCreateView(CreateView):
-    model = Agenda
-    form_class = AgendaForm
-    success_url = reverse_lazy('core:index')
-
 def mes(request):
       query = Agenda.objects.all()
-      context = {"fechas": query}
-      return render(request, "core/month-view.html", context)
+      if request.method == 'GET':
+            form = AgendaForm()
+            context = {'form': form, 'fechas': query}
+            return render(request, 'core/meses.html', context)
+      else:
+            form = AgendaForm(request.POST)
+            if form.is_valid():
+                  form.save()
+            return redirect('core:mes')
+
+def prueba(request):
+     return render(request, "core/selectable.html")
